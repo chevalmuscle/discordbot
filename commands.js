237d552dfe81da03e3165e33a506e41815749bbc
chevalmuscle <marcantoinejean1997@gmail.com>
2019-07-12 20:52:38 -0400

    added play sound command

--- commands.js
+++ commands.js
@@ -1,3 +1,7 @@
+const MongoClient = require("mongodb").MongoClient;
+const mongoConnectionUri = process.env.MONGO_CONNECTION_URI;
+const ytdl = require("ytdl-core");
+
 const commands = {
   help: {
     invocation: "!help",
@@ -25,10 +29,54 @@ const commands = {
 
 function help({ textCommand }) {}
 
-async function playMusic({ message, voiceChannel }) {}
+async function playMusic({ message, voiceChannel }) {
+  const command = message[0];
+
+  if (voiceChannel) {
+    const link = await getMusicLink(command, voiceChannel);
+
+    const streamOptions = { seek: 0, volume: 1 };
+
+    if (isUpperCase(command)) {
+      streamOptions.volume = 10;
+    }
+
+    voiceChannel.join().then(connection => {
+      const stream = ytdl(link, {
+        filter: "audioonly",
+      });
+      const dispatcher = connection.playStream(stream, streamOptions);
+      dispatcher.on("end", end => {
+        voiceChannel.leave();
+      });
+    });
+  }
+}
 
 function addSound({ message }) {}
 
 function deleteSound({ message }) {}
+
+async function getMusicLink(command) {
+  const client = await MongoClient.connect(mongoConnectionUri, {
+    useNewUrlParser: true,
+  }).catch(err => {
+    console.log(err);
+  });
+
+  if (!client) {
+    return;
+  }
+  const collection = client.db("commands").collection("musics");
+  const data = await collection.findOne({ command: command });
+
+  client.close();
+
+  return data.link;
+}
+
+function isUpperCase(str) {
+  return str === str.toUpperCase();
+}
 
 module.exports = commands;
