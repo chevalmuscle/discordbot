cd40047453afce12588cd81f5e84686cf8e01291
chevalmuscle <marcantoinejean1997@gmail.com>
2019-07-12 21:19:57 -0400

    Added addSound command

--- commands.js
+++ commands.js
@@ -30,7 +30,9 @@ const commands = {
 async function help({ textChannel }) {
   const musicCommands = await getMusicCommands();
   textChannel.send("By Chevalmusclé");
-  textChannel.send(musicCommands.map(musicCommand => musicCommand.command).join(", "))
+  textChannel.send(
+    musicCommands.map(musicCommand => musicCommand.command).join(", "),
+  );
 }
 
 async function playMusic({ message, voiceChannel }) {
@@ -57,7 +59,16 @@ async function playMusic({ message, voiceChannel }) {
   }
 }
 
-function addSound({ message }) {}
+function addSound({ message, textChannel }) {
+  const soundCommand = message[1];
+  const link = message[2];
+
+  checkIfCommandInDB(soundCommand, link)
+    .then(addSoundToDB(soundCommand, link))
+    .catch(err =>
+      textChannel.send(`This command already exists with ${err.link}`),
+    );
+}
 
 function deleteSound({ message }) {}
 
@@ -93,6 +104,55 @@ async function getMusicLink(command) {
   client.close();
 
   return data.link;
+}
+
+async function addSoundToDB(soundCommand, link) {
+  const client = await MongoClient.connect(mongoConnectionUri, {
+    useNewUrlParser: true,
+  }).catch(err => {
+    console.log(err);
+  });
+
+  if (!client) {
+    return;
+  }
+  const collection = client.db("commands").collection("musics");
+  await collection.insertOne(
+    {
+      command: soundCommand,
+      link: link,
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+    },
+  );
+
+  client.close();
+}
+
+async function checkIfCommandInDB(soundCommand) {
+  return new Promise(async (resolve, reject) => {
+    const client = await MongoClient.connect(mongoConnectionUri, {
+      useNewUrlParser: true,
+    }).catch(err => {
+      console.log(err);
+    });
+
+    if (!client) {
+      return;
+    }
+    const collection = client.db("commands").collection("musics");
+    const data = await collection.findOne({ command: soundCommand });
+
+    if (data === null) {
+      resolve();
+    } else {
+      reject(data);
+    }
+
+    client.close();
+  });
 }
 
 function isUpperCase(str) {
